77099ec1e47751f1b199295041fec4a0c67252dc
Bartosz Berdychowski <b.berdychowski@gmail.com>
2019-09-25 14:04:37 +0200

    first views

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,18 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
+const spritesmith = require('gulp.spritesmith');
+const merge = require('merge-stream');
+const buffer = require('vinyl-buffer');
+const svgSprite = require('gulp-svg-sprite');
+const imageminSvgo = require('imagemin-svgo');
 
-gulp.task('sass', () => {
+gulp.task('sass', function () {
     return gulp.src('./sass/main.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -16,12 +23,79 @@ gulp.task('sass', () => {
         .pipe(gulp.dest('./css'))
 });
 
-gulp.task('watch', () => {
-    gulp.watch('./sass/*/*.scss', gulp.series('sass'));
+gulp.task('spritepng', function () {
+    var spriteData = gulp.src('./images/_sprites-png/*.png').pipe(spritesmith({
+        imgName: 'sprite-png.png',
+        cssName: '_sprite-png.scss',
+        padding: 20,
+        cssOpts: {
+            functions: false
+        },
+        cssVarMap: function (sprite) {
+            sprite.name = 'spritepng-' + sprite.name;
+        }
+    }));
+
+    var imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(imagemin())
+        .pipe(gulp.dest('./images/'));
+     
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./sass/template/'));
+ 
+    return merge(imgStream, cssStream);
+
 });
 
-gulp.task('imagemin', () => {
-    gulp.src('./images/_source/*.+(png|jpg)')
+gulp.task('spritesvg', function () {
+    return gulp.src('./images/_sprites-svg/*.svg').pipe(svgSprite({
+        shape: {
+            dimension: {
+                maxWidth: 50,
+                maxHeight: 50
+            },
+            spacing: {
+                padding: 10
+            }
+        },
+        mode: {
+            css : {
+                bust: false,
+                dest : '.',
+                sprite : './images/sprite-svg.svg',
+                render : {
+                    scss : {
+                        dest : './sass/_sprite.scss'
+                    }
+                },
+            }
+        }
+    }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('./images/_source/*.+(png|jpg)')
         .pipe(imagemin())
         .pipe(gulp.dest('./images'))
+});
+
+gulp.task('imageminsvg', function () {
+    return gulp.src('./images/_source/*.+(svg)')
+        .pipe(imagemin({
+            use: [
+                imageminSvgo({
+                    plugins: [
+                        {removeViewBox: false}
+                    ]
+                })
+            ]
+        }))
+        .pip
+        .pipe(gulp.dest('./images'))
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./sass/*/*.scss', gulp.series('sass'));
 });
